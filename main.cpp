fd61d2972be00d6d8ca62849db8331024eb23a28
Jim Eli <31859133+JimEli@users.noreply.github.com>
2022-04-02 15:28:09 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,70 +1,7 @@
 #include <iostream>
-#include "RBTree.h"
-#include "Map.h"
+#include "rbtree.h"
+#include "map.h"
 
-/*
-template<typename T>
-struct GenericCompare 
-{
-    int operator ()(const T& left, const T& right) const 
-    {
-        if (left < right) 
-            return -1;
-        else if (left > right)
-            return 1;
-        else 
-            return 0;
-    }
-};
-*/
-/*
-using DataNode_t = RBTreeNode<int>;
-using RBTree_t = RBTree<DataNode_t, Compare_t>;
-
-void PrintRBTree(RBTree_t& rbTree) 
-{
-    std::cout << "PrintRBTree\n";
-    std::cout << "Size: " << rbTree.size() << "\n";
-    
-    DataNode_t* node = rbTree.first();
-    while (!isNull(node)) 
-    {
-        std::cout << "Value: " << node->_value << "\n";
-        node = rbTree.forward(node);
-    }
-}
-
-void TestRBTree() 
-{
-    std::cout << "PrintRBTree\n";
-
-    DataNode_t a, b, c;
-    a._value = 1;
-    b._value = 3;
-    c._value = 2;
-
-    RBTree<DataNode_t, Compare_t> rbTree;
-    rbTree.insertUnique(&a);
-    rbTree.insertUnique(&b);
-    rbTree.insertUnique(&c);
-
-    PrintRBTree(rbTree);
-
-    auto first = rbTree.first();
-    auto last = rbTree.last();
-    auto target = rbTree.search(2);
-    auto backward = rbTree.backward(target);
-    auto forward = rbTree.forward(target);
-
-    rbTree.remove(&c);
-    first = rbTree.first();
-    last = rbTree.last();
-
-    PrintRBTree(rbTree);
-}
-*/
-
-//void PrintMap(Map<int, int, GenericCompare<int>>& map)
 void PrintMap(Map<int, int>& map)
 {
     std::cout << "PrintMap\n";
@@ -78,7 +15,6 @@ void TestMap()
 {
     std::cout << "TestMap\n";
 
-//    Map<int, int, GenericCompare<int>> map;
     Map<int, int> map;
     map.insert(1, 101);
     map.insert(1, 1001);
@@ -105,7 +41,6 @@ void TestMap()
 
 int main() 
 {
-//    TestRBTree();
     TestMap();
 
     return 0;
